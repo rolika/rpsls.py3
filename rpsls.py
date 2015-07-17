70c3313a8e6041b641eee64283802d5f5921f293
rolika <weisz.roland@pohlen-dach.hu>
2015-07-17 22:24:51 +0200

    rewritten for simplicity

--- rpsls.py
+++ rpsls.py
@@ -4,8 +4,13 @@
 
 import random
 
-def getWinner(players_choice, computers_choice, rules):
-    """ Returns winner party """
+def getWinner(players_choice, computers_choice):
+    """ Returns winner party based on rules"""
+    rules = ("Scissors cuts Paper", "Paper covers Rock", "Rock crushes Lizard",
+             "Lizard poisions Spock", "Spock smashes Scissors",
+             "Scissors decapitates Lizard", "Lizard eats Paper",
+             "Paper disproves Spock", "Spock vaporizes Rock",
+             "Rock crushes Scissors") #these are rules of game
     for rule in rules: #key idea: in rules, first word wins, last word loses
         winner, loser = rule.split()[0].lower(), rule.split()[-1].lower()
         if players_choice == winner and computers_choice == loser:
@@ -15,35 +20,21 @@ def getWinner(players_choice, computers_choice, rules):
 
 def rpsls(players_choice):
     """ Based on argument decides who wins round """
-    rules = ("Scissors cuts Paper",
-             "Paper covers Rock",
-             "Rock crushes Lizard",
-             "Lizard poisions Spock",
-             "Spock smashes Scissors",
-             "Scissors decapitates Lizard",
-             "Lizard eats Paper",
-             "Paper disproves Spock",
-             "Spock vaporizes Rock",
-             "Rock crushes Scissors") #these are rules of game
-    valid_words = [rule.split()[0].lower() for rule in rules[:5]] #extracts valid game words
+    valid_words = ("rock", "paper", "scissors", "lizard", "spock") #game words
     computers_choice = random.choice(valid_words)
-
     if players_choice.lower() in valid_words: #on valid input
-        print("Player chooses", players_choice.capitalize()+".") #display choices
-        print("Computer chooses", computers_choice.capitalize()+".")
-
+        print("Player chooses", players_choice) #display choices
+        print("Computer chooses", computers_choice)
         if players_choice == computers_choice: #same entry
             print("It's a draw!\n")
-            return None #returns None
-
-        winner, rule = getWinner(players_choice, computers_choice, rules)
-
+            return None #returns None    
+        winner, rule = getWinner(players_choice, computers_choice)
         print(rule + "!") #game description
         print(winner, "wins!\n") #winner
         return winner
-
-    print("Invalid input!")
-    return None
+    else:
+        print("Invalid input!")
+        return None
 
 rpsls("rock")
 rpsls("paper")
