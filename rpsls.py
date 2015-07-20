ab79010c0902c523c13e701bdc7c111edf151654
Weisz Roland <weisz.roland@pohlen-dach.hu>
2015-07-20 11:59:34 +0200

    rewritten to match course spec

--- rpsls.py
+++ rpsls.py
@@ -4,41 +4,40 @@
 
 import random
 
-def getWinner(players_choice, computers_choice):
+def rpsls(players_choice):
     """ Returns winner party based on rules"""
     rules = ("Scissors cuts Paper", "Paper covers Rock", "Rock crushes Lizard",
              "Lizard poisions Spock", "Spock smashes Scissors",
              "Scissors decapitates Lizard", "Lizard eats Paper",
              "Paper disproves Spock", "Spock vaporizes Rock",
              "Rock crushes Scissors") #these are rules of game
-    for rule in rules: #key idea: in rules, first word wins, last word loses
-        winner, loser = rule.split()[0].lower(), rule.split()[-1].lower()
-        if players_choice == winner and computers_choice == loser:
-            return "Player", rule
-        elif computers_choice == winner and players_choice == loser:
-            return "Computer", rule
-
-def rpsls(players_choice):
-    """ Based on argument decides who wins round """
-    valid_words = ("rock", "paper", "scissors", "lizard", "spock") #game words
+    valid_words = [rule.split()[0].lower() for rule in rules[:5]]
     computers_choice = random.choice(valid_words)
-    if players_choice.lower() in valid_words: #on valid input
-        print("Player chooses", players_choice) #display choices
-        print("Computer chooses", computers_choice)
-        if players_choice == computers_choice: #same entry
-            print("It's a draw!\n")
-            return None #returns None    
-        winner, rule = getWinner(players_choice, computers_choice)
-        print(rule + "!") #game description
-        print(winner, "wins!\n") #winner
+    if players_choice in valid_words:
+        print("\nPlayer chooses:  ", players_choice.capitalize())
+        print("Computer chooses:", computers_choice.capitalize())
+        if players_choice == computers_choice:
+            print("Player and Computer tie!")
+            return None
+        #key idea: in rules, first word wins, last word loses
+        #works only with valid inputs and not-equal choices
+        for rule in rules:
+            winner, loser = rule.split()[0].lower(), rule.split()[-1].lower()
+            if players_choice == winner and computers_choice == loser:
+                winner = "Player"
+                break
+            elif computers_choice == winner and players_choice == loser:
+                winner = "Computer"
+                break
+        print(rule)
+        print(winner, "wins!")
         return winner
-    else:
-        print("Invalid input!")
-        return None
+    print("\nInvalid input!")
+    return None
 
 rpsls("rock")
 rpsls("paper")
 rpsls("scissors")
+rpsls("something")
 rpsls("lizard")
 rpsls("spock")
-rpsls("something else")
